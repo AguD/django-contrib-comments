6320b7e1851faa844059dcefd999db06b6e28c81
Agustin Dahbar <agud.cba@gmail.com>
2014-07-04 00:28:17 -0300

    First approach to handle comments on comments

--- django_comments/static/js/comment.js
+++ django_comments/static/js/comment.js
@@ -15,7 +15,8 @@ function post_comment(e){
         comment_field = form.find("#id_comment"),
         comment = comment_field.val(),
         subButton = form.find(':input[type="submit"]'),
-        comments_box = form.parents(".comments-box").find(".comments-list");
+        cid = $(this).closest('.comment').attr('id').slice(1),
+        comments_box = $('#comment-list'+cid);
 
     e.preventDefault();
     comment = replace_breaklines(comment);
@@ -47,13 +48,23 @@ function post_comment(e){
 
 $(document).ready(function() {
     $(".container").on({
-        mouseenter: function() {
-            $(this).find(".vote_buttons").addClass("in");
-        }, mouseleave: function() {
-            $(this).find(".vote_buttons").removeClass("in");
+        mouseenter: function(e) {
+            $(this).find(".vote_buttons:first").addClass("in");
+            e.stopPropagation();
+        }, mouseleave: function(e) {
+            // We dont stop propagation here! Mouseenter the parent, 
+            //mouseenter the child, mouseleave it, and we miss the parent's mouseleave
+            $(this).find(".vote_buttons:first").removeClass("in");
         }
     }, ".comments-list .comment")
 
     // SUBMIT COMMENT
     $(".container").on("submit", ".comment-form", post_comment);
+    $(".container").on("click", ".comment .answers", function(e){
+        cid = $(this).closest('.comment').attr('id').slice(1)
+        // Show the form to answer a comment
+        $('#comment-form'+cid).removeClass('hidden').addClass('in');
+        $('#comment-list'+cid).removeClass('hidden').addClass('in').focus();
+        e.preventDefault();
+    });
 });
